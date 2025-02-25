624b74dffc774b02ce245bef70e76f2fc2d0b754
Xoshimov_2602 <xoshimovabdurazzoq@gmail.com>
2025-02-25 23:26:17 +0500

    added markets api

--- lib/data/source/remote/api/info/info_api.dart
+++ lib/data/source/remote/api/info/info_api.dart
@@ -1,10 +1,7 @@
-
 import 'package:dio/dio.dart';
 import 'package:retrofit/error_logger.dart';
 import 'package:retrofit/http.dart';
 import 'package:texnomart_clone/data/source/remote/response/info/info_response.dart';
-
-
 part 'info_api.g.dart';
 
 @RestApi()
